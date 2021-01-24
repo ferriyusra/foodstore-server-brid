5103c3054d60e8173c60ebf3491b82acf8953cc1
John Smith <ferisakuragi@gmail.com>
2021-01-24 18:24:16 +0700

    api wilayah
    Membuat Endpoint Get Kabupaten Berdasarkan
    Provinsi ~
    Membuat Endpoint Get Kecamatan Berdasarkan
    Kabupaten ~
    Membuat Endpoint Get Desa Berdasarkan
    Kecamatan ~

--- app/wilayah/controller.js
+++ app/wilayah/controller.js
@@ -6,7 +6,9 @@ async function getProvinsi(req, res, next) {
 
     try {
         const data = await csv().fromFile(db_provinsi)
+
         return res.json(data)
+
     } catch (err) {
         return res.json({
             error: 1,
@@ -15,6 +17,68 @@ async function getProvinsi(req, res, next) {
     }
 }
 
+async function getKabupaten(req, res, next) {
+    const db_kabuapten = path.resolve(__dirname, './data/regencies.csv')
+
+    try {
+        let { kode_induk } = req.query
+
+        const data = await csv().fromFile(db_kabuapten)
+
+        return res.json(data.filter(kabupaten => kabupaten.kode_provinsi === kode_induk))
+
+    } catch (err) {
+        return res.json({
+            error: 1,
+            message: `Tidak bisa mengambil data kabupaten, hubungi administrator`
+        })
+    }
+}
+
+async function getKecamatan(req, res, next) {
+    const db_kecamatan = path.resolve(__dirname, './data/districts.csv')
+
+    try {
+        let { kode_induk } = req.query
+
+        const data = await csv().fromFile(db_kecamatan)
+
+        if (!kode_induk) return res.json(data)
+
+        return res.json(data.filter(kecamatan => kecamatan.kode_kabupaten === kode_induk))
+
+    } catch (err) {
+        return res.json({
+            error: 1,
+            message: `Tidak bisa mengambil data kecamatan, hubungi administrator`
+        })
+    }
+}
+
+async function getDesa(req, res, next) {
+
+    try {
+
+        const db_desa = path.resolve(__dirname, './data/villages.csv')
+
+
+        let { kode_induk } = req.query
+        const data = await csv().fromFile(db_kecamatan)
+
+        if (!kode_induk) return res.json(data)
+
+        return res.json(data.filter(desa => desa.kode_kecamatan === kode_induk))
+    } catch (err) {
+        return res.json({
+            error: 1,
+            message: `Tidak bisa mengambil data desa, hubungi administrator`
+        })
+    }
+}
+
 module.exports = {
-    getProvinsi
+    getProvinsi,
+    getKabupaten,
+    getKecamatan,
+    getDesa
 }
