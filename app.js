8cf0d9e858ac4afc007f526106e7a7a1561cad19
John Smith <ferisakuragi@gmail.com>
2021-01-26 20:50:28 +0700

    mengatasi cors
    - menginstal cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 // import file product router
 const productRouter = require('./app/product/router')
@@ -48,6 +49,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// gunakan cors
+app.use(cors())
 
 // gunakan middleware decodeToken
 app.use(decodeToken());
